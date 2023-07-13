22d97addec560777bc1838a639d733ebf3cae1a0
Bruno de Assunção <Brunodeassuncao03@hotmail.com>
2023-07-12 22:45:38 -0300

    commit nada

--- index.php
+++ index.php
@@ -4,13 +4,13 @@
       <title>Area de login</title>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <link rel="stylesheet" href="../css/area_login.css">
-      <link rel="icon" href="../imagens/icons8-login-50.png">
+      <link rel="stylesheet" href="css/area_login.css">
+      <link rel="icon" href="imagens/icons8-login-50.png">
    </head>
    <body>
       <div class="form">
          <p id="heading">Login</p>
-         <form method="POST" action="../php/processo_cliente.php" enctype="multipart/form-data">
+         <form method="POST" action="php/processo_cliente.php" enctype="multipart/form-data">
             <input type="hidden" name="type" value="login" required>    <!--parte php q vc fez -->
             <div class="field">
                <input autocomplete="off" placeholder="Username" id="login" name="login" class="input-field" type="text" id="">
@@ -20,11 +20,15 @@
             </div>
             <div class="btn">
                <button type="submit" class="button1">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Login&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</button>
-               <button type="button" class="button2" href="criar_conta.html">Increva-se</button>
+               <button type="button" class="button2" href="html/criar_conta.html">Inscreva-se</button>
             </div>
+<<<<<<< Updated upstream
             <butto type="button"  class="button3" href="esqueceu_senha.html">
+=======
+            <button type="button"  class="button3" href="html/esqueceu_senha.html">
+>>>>>>> Stashed changes
             Esqueceu a Senha</button>
-            <button type="button"  class="button4" href="catalogo.html">Entrar como visitante</button>
+            <button type="button"  class="button4" href="html/catalogo.html">Entrar como visitante</button>
          </form>
       </div>
       <script src="script.js"></script>
