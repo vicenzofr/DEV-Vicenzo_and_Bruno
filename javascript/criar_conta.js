b73efc7d38587757bd70660419bb3f6afec99bcd
Vicenzo <vicenzofr@gmail.com>
2023-07-23 16:17:53 -0300

    nao quer funcionar a checkbox

--- javascript/criar_conta.js
+++ javascript/criar_conta.js
@@ -11,15 +11,25 @@ function getPassword() {
   }
 
 
-  const password = document.getElementById('password');
-  const icon = document.getElementById('icon');
+  // const password = document.getElementById('password');
+  // const icon = document.getElementById('icon');
   
-  function showHide() {
-    if (password.type === 'password') {
-      password.setAttribute('type', 'text');
-      icon.classList.add('hide');
+  // function showHide() {
+  //   if (password.type === 'password') {
+  //     password.setAttribute('type', 'text');
+  //     icon.classList.add('hide');
+  //   } else {
+  //     password.setAttribute('type', 'password');
+  //     icon.classList.remove('hide');
+  //   }
+  // }
+
+
+  function mostrarOcultarSenha() {
+    var senha = document.getElementById("senha");
+    if (senha.type === "password") {
+        senha.type = "text";
     } else {
-      password.setAttribute('type', 'password');
-      icon.classList.remove('hide');
+        senha.type = "password";
     }
-  }
+}
