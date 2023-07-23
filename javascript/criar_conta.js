20fc7f68294004c2e2ca8bbe0a55e9b0291b0925
Vicenzo <vicenzofr@gmail.com>
2023-07-23 15:58:48 -0300

    checkbox incompleta

--- javascript/criar_conta.js
+++ javascript/criar_conta.js
@@ -9,3 +9,17 @@ function getPassword() {
     }
     document.getElementById('senha').value = password
   }
+
+
+  const password = document.getElementById('password');
+  const icon = document.getElementById('icon');
+  
+  function showHide() {
+    if (password.type === 'password') {
+      password.setAttribute('type', 'text');
+      icon.classList.add('hide');
+    } else {
+      password.setAttribute('type', 'password');
+      icon.classList.remove('hide');
+    }
+  }
