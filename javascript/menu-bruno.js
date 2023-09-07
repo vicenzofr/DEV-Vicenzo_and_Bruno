9a481f9e3ec6a64f4a60d203ccdeb70e0b7d8fbb
Bruno de Assunção <Brunodeassuncao03@hotmail.com>
2023-09-07 00:21:33 -0300

    Commit com o professor
    Presente professor! muito obrigado

--- javascript/menu-bruno.js
+++ javascript/menu-bruno.js
@@ -1,16 +1,16 @@
-let btnMenu= document.getElementById('btn-menu')
-let Menu=document.getElementById('menu-mobile')
-let Overlay=document.getElementById('overlay-menu')
+let btnMenu = document.getElementById('btn-menu');
+let menu = document.getElementById('menu-mobile');
+let overlay = document.getElementById('overlay-menu');
 
-btnMenu.addEventListener('click',()=>{
-    Menu.classList.add('abrir-menu')
+btnMenu.addEventListener('click', function () {
+    menu.classList.add('abrir-menu');
 })
 
-Menu.addEventListener('click',()=>{
-    Menu.classList.remove('abrir-menu')
+menu.addEventListener('click',()=>{
+    menu.classList.remove('abrir-menu');
 })
 
-Overlay.addEventListener('click',()=>{
-    Menu.classList.remove('abrir-menu')
+overlay.addEventListener('click',()=>{
+    menu.classList.remove('abrir-menu');
 })
 
